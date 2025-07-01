dbd8773fe56a95626d3be22f2c310197b2935b57
Kaushikmak <96010041+Kaushikmak@users.noreply.github.com>
2025-07-01 20:11:14 +0530

    first screen using OpenGL

--- main.cpp
+++ main.cpp
@@ -1,5 +1,46 @@
+#include <cstdio>
 #include <iostream>
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
+using namespace std;
 
 int main() {
+    if (!glfwInit()) {
+        cerr << "Failed to initialize GLFW" << endl;
+        return -1;
+    }
+
+    // Set OpenGL version (optional but recommended)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "OPENGL window", nullptr, nullptr);
+    if (!window) {
+        cerr << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK) {
+        cerr << "Failed to initialize GLEW" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    cout << "OpenGL Version: " << glGetString(GL_VERSION) << endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
